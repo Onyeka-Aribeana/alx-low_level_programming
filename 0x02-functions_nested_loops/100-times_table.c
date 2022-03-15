38a8c05a7eac005aa042dcc711382a3bde9f7152
Onyeka-Aribeana <oaribeana@gmail.com>
2022-03-15 22:30:38 +0000

    Added a function that prints the n times table, starting with 0

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+void print_times_table(int);
+/**
+ * print_times_table - prints absolute of number
+ * @n: number
+ *
+ * Description: does my stuff
+ * Return: 1 or 0
+ */
+void print_times_table(int n)
+{
+int i, j;
+if (n <= 15 && n >= 0)
+{
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+if (j != n)
+{
+printf("%d,   ", i * j);
+}
+else
+{
+printf("%d\n", i * j);
+}
+}
+}
+}
+}
