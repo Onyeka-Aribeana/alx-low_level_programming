bcc9a46b9dee0e049e3bc5b3dccf39764208554c
Onyeka-Aribeana <oaribeana@gmail.com>
2022-03-16 01:17:59 +0000

    Added a program that finds and prints the sum of the even-valued terms, followed by a new line

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+long int i = 1, j = 2, k = 0, m;
+while (true)
+{
+m = j;
+j = j + i;
+i = m;
+if (j >= 4000000)
+{
+break;
+}
+else
+{
+if (j % 2 == 0)
+{
+k = k + j;
+}
+}
+printf("%ld\n", k + 2);
+}
+return (0);
+}
