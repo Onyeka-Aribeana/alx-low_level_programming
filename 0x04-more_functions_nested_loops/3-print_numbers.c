1064966e28fdc18538adc2a31783bd172c616e89
Onyeka-Aribeana <oaribeana@gmail.com>
2022-03-17 19:25:25 +0000

    Added a function that prints the numbers, from 0 to 9, followed by a new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - checks for uppercase letters
+ *
+ * Description: does my stuff
+ * Return: 1 or 0
+ */
+void print_numbers(void)
+{
+int n;
+for (n = 48; n <= 57; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
+}
