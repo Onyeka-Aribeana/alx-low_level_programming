f4ed4d92d5f93571a9fd0f25242aacb9456e3837
Onyeka-Aribeana <oaribeana@gmail.com>
2022-03-17 19:35:42 +0000

    Added a function that prints the numbers, from 0 to 9, followed by a new line

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,12 +2,12 @@
 #include "main.h"
 
 /**
- * print_numbers - checks for uppercase letters
+ * print_most_numbers - checks for uppercase letters
  *
  * Description: does my stuff
  * Return: 1 or 0
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 int n;
 for (n = 48; n <= 57; n++)
