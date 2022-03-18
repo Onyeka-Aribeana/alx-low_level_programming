f56cd6834584c2ce6c2b1e2fd0482d491050e971
Onyeka-Aribeana <oaribeana@gmail.com>
2022-03-18 23:22:13 +0000

    Fixed second advanced question

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,44 +1,24 @@
-#include <stdio.h>
-#include <math.h>
 #include "main.h"
-
-
 /**
- * print_number - prints in string
- * @n: number
- *
- *
- * Description: does my stuff
- * Return: 1 or 0
+ * print_number - prints number
+ * @n: integer to be printed
  */
-
-
 void print_number(int n)
 {
-int i, j, k, l;
+unsigned int j;
+
 if (n < 0)
 {
+j = -n;
 _putchar('-');
-n = n * -1;
-}
-else if (n == 0)
-{
-_putchar(48);
-}
-k = log10(n) + 1;
-for (i = 0; i < k; i++)
-{
-j = log10(n) + 1;
-if (j < k - i)
-{
-l = 0;
 }
 else
 {
-l = n / pow(10, j - 1);
+j = n;
 }
-_putchar(l + 48);
-n = n - (l *pow(10, j - 1));
+if (j / 10)
+{
+print_number(j / 10);
 }
-_putchar('\n');
+_putchar((j % 10) + '0');
 }
