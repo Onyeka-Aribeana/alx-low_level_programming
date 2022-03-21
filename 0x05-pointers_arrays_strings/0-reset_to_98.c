a5c302c3fd9c7ea8bfca058ea0e7bd87c79f557a
Onyeka-Aribeana <oaribeana@gmail.com>
2022-03-21 23:07:47 +0000

    Added new folder for 0x05, README and a function that takes a pointer to an int as parameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - resets variable to 98
+ * @n: number
+ *
+ * Description: do my stuff
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
