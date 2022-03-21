9241077bb617474e3bb85ebd4e6fdce750d9c64e
Onyeka-Aribeana <oaribeana@gmail.com>
2022-03-21 23:48:01 +0000

    Added a function that prints a string, followed by a new line, to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - return length of string
+ * @str: char array
+ *
+ * Description: do stuff
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+int u;
+u = 0;
+while (s[u] != '\0')
+{
+_putchar(s[u]);
+u++;
+}
+_putchar('\n');
+return (u);
+}
