bff6116707eb4ccf902d4d8ced1c75012bfcb098
Onyeka-Aribeana <oaribeana@gmail.com>
2022-03-24 15:06:44 +0000

    Added a function that reverses the content of an array of integers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - resets variable to 98
+ * @a: arr
+ * @n: number
+ *
+ * Description: do my stuff
+ * Return: Always 0.
+ */
+void reverse_array(int *a, int n)
+{
+int i, j;
+int dest[n];
+for (i = 0; i < n; i++)
+{
+dest[i] = a[i];
+}
+for (j = 0; j < n; j++)
+{
+a[j] = dest[n - j - 1];
+}
+}
